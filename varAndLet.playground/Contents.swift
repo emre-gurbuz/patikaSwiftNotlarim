63039ed0fbf9d5b82d7809feb3e51d026b6d7164
hüseyin emre gürbüz <01.hsyngrbz@gmail.com>
2023-07-08 00:18:14 +0300

    1.drs OK notlar ghuba yüklendi

---
+++ varAndLet.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+// değişken tanımlama
+var greeting = "Hello, playground "
+
+// tipli değişken tanımlama
+var greeting1: String = "Hello, playground"
+
+//sabit tanımlama double
+let pi = 3.14
+
+// tipli sabit float
+let pi2: Float = 3.14
+
+//
+let isim = "hüseyin emre gürbüz"
+
+//cgfloat grafik işlemlerinde kullanılır
+let pi3:CGFloat=3.14
+
+//içi boş değişken
+var bosluk: String
+
+// bos sabit
+let bossluk: Int
+
+// ekrana yazdırma
+print(greeting + isim)
+print(isim)
+
+// değikenin değer ataması
+greeting = "merhaba "
+
+//String ploytion stringin içine değer gömmek
+print("hello \(isim)")
+print("\(greeting) \(isim)")
+
+//arrayler
+
+//int liste
+var yaslar = [1,2,34,5,6,7]
+
+// string liste
+var isimler = ["ali","ahmet","mehmet","keriman"]
+
+// array yazdırma
+print("\(isimler[1]), \(yaslar[2])")
+
+
